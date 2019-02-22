5a8a723f4762259c9b46d0b3267465f9a490561b
Anton Babenko <anton@antonbabenko.com>
2019-02-22 15:16:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Complete Terraform documentation as PDF files. Updating nightly.
+
+[![CircleCI](https://circleci.com/gh/antonbabenko/terraform-docs-as-pdf.svg?style=svg&circle-token=9e5321eb98d9d47cda7ef4209e287f56c25a702a)](https://circleci.com/gh/antonbabenko/terraform-docs-as-pdf)
+![](https://img.shields.io/github/repo-size/antonbabenko/terraform-docs-as-pdf.svg)
+![](https://img.shields.io/github/last-commit/antonbabenko/terraform-docs-as-pdf.svg)
+
+> Once upon a time, I've been missing Terraform documentation on my flight across Atlantic.
+
+## Features
+
+1. Official content from [terraform.io](https://terraform.io/) web-site is used. Source: [terraform-website](https://github.com/hashicorp/terraform-website).
+1. Include documentation for 95+ officially supported Terraform providers. Source: [terraform-providers](https://github.com/terraform-providers).
+1. Updating nightly.
+1. Documentation is generated from the web-site running locally.
+1. Get updates in your Dropbox folder - https://www.dropbox.com/sh/pfj9e1m5d7nqhp3/AAAqW1OI3oYPa4UXfYb4fLGaa
+
+## How to use this?
+
+1. Don't have Dropbox account? Open one using [my referral link](https://db.tt/5Bhs91K9Wb) and I will get 500M extra. :)
+
+1. To get updates automatically to your Dropbox folder - click [this shared link](https://www.dropbox.com/sh/pfj9e1m5d7nqhp3/AAAqW1OI3oYPa4UXfYb4fLGaa), then choose "Download" and "Save to my Dropbox".
+
+1. Alternatively, browse PDFs online - [https://www.dropbox.com/sh/pfj9e1m5d7nqhp3/AAAqW1OI3oYPa4UXfYb4fLGaa](https://www.dropbox.com/sh/pfj9e1m5d7nqhp3/AAAqW1OI3oYPa4UXfYb4fLGaa)
+
+## Existing PDF files
+
+1. All in one: Terraform docs with all official providers
+1. Terraform docs (without providers)
+1. Terraform docs for each provider
+
+## Authors
+
+This project is maintained by [Anton Babenko](https://github.com/antonbabenko) with help from [these awesome contributors](https://github.com/antonbabenko/terraform-docs-as-pdf/graphs/contributors).
+
+## License
+
+MIT Licensed. See LICENSE for full details.
+
+Terraform documentation itself is licensed under MPL 2.0. See [LICENSE.md](https://github.com/hashicorp/terraform-website/blob/master/LICENSE.md) for more details.
+
---
+++ bin/generate.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+
+TARGET_SITE="http://localhost:4567"
+
+# Get all urls
+# wget --spider --force-html -r -l5 "$TARGET_SITE" 2>&1 | grep '^--' | awk '{ print $3 }' | grep -v '\.\(css\|js\|png\|gif\|jpg|svg|woff|ttf|zip|json\)$' > url-list-5.txt
+
+# Using sitemap (better)
+# curl -O http://localhost:4567/sitemap.xml
+
+# gnu grep
+#ggrep -Po 'http(s?)://[^ \"()\<>]*' sitemap.xml
+
+# Remove index.html from URL
+#sed -i -r "s#/index.html\$##g" url-list-5.txt
+
+# Make list unique and nice
+#sort url-list-5.txt | uniq | sort -n > unique-5.txt
+
+URLS="unique-urls.txt"
+
+CMDS_DIR="cmds"
+
+function assert_is_installed {
+  local readonly name="$1"
+
+  if [[ ! $(command -v ${name}) ]]; then
+    echo "ERROR: The binary '$name' is required by this script but is not installed or in the system's PATH."
+    exit 1
+  fi
+}
+
+function string_starts_with {
+  local readonly str="$1"
+  local readonly prefix="$2"
+
+  [[ "$str" == "$prefix"* ]]
+}
+
+function verify_dependencies {
+
+  assert_is_installed git
+  assert_is_installed curl
+  assert_is_installed grep # gnu
+#  assert_is_installed ggrep
+  assert_is_installed sed
+
+  # https://wkhtmltopdf.org/downloads.html
+  assert_is_installed wkhtmltopdf
+
+  # https://www.ghostscript.com/download.html
+  assert_is_installed gs
+}
+
+function prepare_urls {
+  # Get all URLs from sitemap.xml
+  # ggrep on Mac = GNU grep
+  curl $TARGET_SITE/sitemap.xml | grep -Po 'http(s?)://[^ \"()\<>]*' | sed -r "s#/index.html\$##g" > all-urls.txt
+
+  # Drop duplicates
+  sort all-urls.txt | uniq | sort -n > $URLS
+
+  # Replace URL to local one
+  sed -i -r "s#https://www.terraform.io#${TARGET_SITE}#g" $URLS
+}
+
+function recreate_work_dir {
+  rm -rf $CMDS_DIR
+  mkdir -p $CMDS_DIR
+
+  rm -rf pdfs
+  mkdir -p pdfs
+}
+
+
+function make_cmd_files {
+
+  i=0
+  index=0
+  prev_group_name=""
+
+  while read url; do
+    url_path=$(echo "$url" | sed -r 's#https?://[a-z0-9\:]+/(.*)#\1#g')
+
+    if $(string_starts_with "$url_path" "docs/providers/"); then
+      provider_name=$(echo "$url_path" | sed -r 's#([a-z]+/){2}([a-z]+)(.*)#\2#g')
+
+      if [[ $prev_group_name != $provider_name ]]; then
+        i=0
+        index=0
+      fi
+
+      echo -n "$url " >> "${CMDS_DIR}/${provider_name}.${index}"
+      prev_group_name=$provider_name
+    else
+      echo -n "$url " >> "${CMDS_DIR}/terraform-website.${index}"
+      prev_group_name="terraform-website"
+    fi
+
+    if (( $i != 0 && $i % 20 == 0 )); then
+      ((index=index+1))
+    fi
+
+    ((i=i+1))
+
+  done < "$URLS"
+}
+
+function make_final_cmd_file {
+
+  for file in $(\ls ${CMDS_DIR}); do
+    echo "pdfs/$file.pdf" >> "${CMDS_DIR}/${file}"
+    cat "${CMDS_DIR}/${file}" >> "${CMDS_DIR}/final"
+  done
+
+}
+
+function make_pdf_files {
+
+  wkhtmltopdf \
+    --print-media-type \
+    --disable-javascript \
+    --disable-internal-links \
+    --javascript-delay 10000 \
+    --load-error-handling skip \
+    --stop-slow-scripts \
+    --read-args-from-stdin \
+  < "${CMDS_DIR}/final"
+
+}
+
+function combine_pdf_files {
+
+  pushd pdfs
+  all_pdfs=($(\ls -1 *.pdf))
+
+  prev_name=""
+  for file in "${all_pdfs[@]}"; do
+    name=$(cut -d "." -f 1 <<< "$file")
+
+    if [[ $prev_name != $name ]]; then
+      echo "Combining PDF files by name - $name"
+
+      gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=../${name}.pdf $(\ls -1 ${name}.*.pdf | sort -V)
+    fi
+
+    prev_name=$name
+  done
+
+  # Make one huge (include terraform-website in the beginning)
+  # @todo: include cover, toc
+  echo "Combining all PDF files into one - complete-terraform-website.pdf"
+  gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=../complete-terraform-website.pdf ../terraform-website.pdf $(\ls -1 ../*.pdf | grep -v terraform-website.pdf | sort -V)
+
+  popd
+
+}
+
+function upload_pdf_files {
+
+  all_pdfs=($(\ls -1 *.pdf))
+
+  for file in "${all_pdfs[@]}"; do
+
+    echo "Uploading ${file}"
+
+    curl -X POST https://content.dropboxapi.com/2/files/upload \
+      --header "Authorization: Bearer $DROPBOX_TOKEN" \
+      --header "Dropbox-API-Arg: {\"path\": \"/terraform-docs-as-pdf/${file}\",\"mode\": \"overwrite\",\"autorename\": false,\"mute\": false,\"strict_conflict\": false}" \
+      --header "Content-Type: application/octet-stream" \
+      --data-binary @$file
+
+    rm -f $file
+
+  done
+
+# List all filenames
+#curl -X POST https://api.dropboxapi.com/2/files/list_folder \
+#    --header "Authorization: Bearer $DROPBOX_TOKEN" \
+#    --header "Content-Type: application/json" \
+#    --data "{\"path\": \"/terraform-docs-as-pdf\",\"recursive\": false,\"include_media_info\": false,\"include_deleted\": false,\"include_has_explicit_shared_members\": false,\"include_mounted_folders\": false, \"limit\": 100}" | jq -r '.entries[] | select (.".tag"|test("file")) | .name' | sort
+
+}
+
+function git_config_commit_and_push {
+
+  echo "Configuring git to use user.email '$GIT_USER_EMAIL', user.name '$GIT_USER_NAME', and 'push.default matching'"
+  git config credential.helper 'cache --timeout=120'
+  git config user.email "$GIT_USER_EMAIL"
+  git config user.name "$GIT_USER_NAME"
+  git config --global push.default matching
+
+  git add .
+  git status
+  git commit -m "[skip ci] Updated documentation"
+  git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/antonbabenko/terraform-docs-as-pdf.git master
+
+}
+
+echo "Verified installed dependencies..."
+verify_dependencies
+
+echo "Preparing list of URLs..."
+prepare_urls
+
+echo "Prepare working directories..."
+recreate_work_dir
+
+echo "Making list of cmd files..."
+make_cmd_files
+
+echo "Combining small cmd files into one..."
+make_final_cmd_file
+
+echo "Making pdf files... This may take some time..."
+make_pdf_files
+
+echo "Combining PDF files..."
+combine_pdf_files
+
+echo "Uploading PDF files..."
+upload_pdf_files
+
+echo "Upload PDF files to github..."
+git_config_commit_and_push
